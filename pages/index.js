41848ae2b5bf28c137758b3100faddac01e3b5d0
Matt Hayes <matt@mysterycommand.com>
2017-05-20 15:34:47 -0400

    feat(TestEditor): adds an editor component

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+import ReactDom from 'react-dom';
+import { Editor, EditorState } from 'draft-js';
+
+export default class TestEditor extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { editorState: EditorState.createEmpty() };
+        this.onChange = editorState => this.setState({ editorState });
+    }
+
+    render() {
+        return (
+            <Editor
+                editorState={ this.state.editorState }
+                onChange={ this.onChange }
+            />
+        );
+    }
+}
